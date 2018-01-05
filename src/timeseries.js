121621d23ce468db92475e8d1ef77aaff4bac70c
Greg Walls <greg.walls@sparkpost.com>
2018-01-05 17:27:45 -0500

    FAD-5695 updated version and added comment around change

--- src/timeseries.js
+++ src/timeseries.js
@@ -114,6 +114,8 @@ TimeSeries.prototype.getHits = function(key, gran, count, callback) {
 
     for(var ts=from, i=0, data=[]; ts<=to; ts+=properties.duration, i+=1) {
       var result = results[i];
+      // this will account for the difference in multi transaction results between ioredis and node_redis
+      // https://github.com/luin/ioredis/wiki/Migrating-from-node_redis
       if (result instanceof Array) {
         if (result[0]) {
           return callback(result[0]);
